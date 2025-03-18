22340552e4f3411d4b8a6fcb48ac92829fb5a590
nayara-oliveira <nayara18276@gmail.com>
2025-03-17 22:02:14 -0300

    :construction: refactor: separei o código escrito em funções, mas ainda não funciona do jeito correto

--- labirinto.c
+++ labirinto.c
@@ -19,6 +19,10 @@ typedef struct{
     int j;
 }posicao;
 
+void armazenar_labirinto(FILE *teste, int m, int n);
+//void printar_labirinto(int m, int n, char labirinto[m][n]);
+void salvar_labirinto(int m, int n, int ordem, char labirinto[m][n]);
+
 //Funcao Main
 int main(){
     int resp,m,n,i,j,a=0,a_anterior;
@@ -38,64 +42,44 @@ int main(){
         scanf("%d",&resp);
     }while(resp!=1 && resp!=2 && resp!=3 && resp!=4);
 
-    //Executa com base na resposta
-    if(resp==1 || resp==2 || resp==3){
+    switch (resp) {
+        case 1: {
+            //Pedindo para o usuario selecionar o arquivo de labirinto a ser carregado
+            printf("%s%s%s",Azul,"Digite o nome do arquivo do labirinto(sem o .txt):\n",NONE);
+            scanf("%s",nome_arquivo);
+            
+            // VS CODE
+            strcpy(aux,"../labirintos/");
+            strcat(aux,nome_arquivo);
+            FILE* teste = fopen(strcat(aux,".txt"),"r");
+            
+            // GEANY
+            // strcpy(aux,"labirintos/");
+            // strcat(aux,nome_arquivo);
+            // FILE* teste = fopen(strcat(aux,".txt"),"r"); // para ler no geany
 
-        //Pedindo para o usuario selecionar o arquivo de labirinto a ser carregado
-        printf("%s%s%s",Azul,"Digite o nome do arquivo do labirinto(sem o .txt):\n",NONE);
-        scanf("%s",nome_arquivo);
-        
-        // VS CODE
-        strcpy(aux,"../labirintos/");
-        strcat(aux,nome_arquivo);
-        FILE* teste = fopen(strcat(aux,".txt"),"r");
-        
-        // GEANY
-        // strcpy(aux,"labirintos/");
-        // strcat(aux,nome_arquivo);
-        // FILE* teste = fopen(strcat(aux,".txt"),"r"); // para ler no geany
-        
-        
-        
-        if(teste==NULL) printf("Nao foi possivel abrir o arquivo\n");
+            if(teste==NULL) printf("Nao foi possivel abrir o arquivo\n");
 
-        //Lendo a primeira linha do arquivo, para criar a matriz com a ordem informada
-        fgets(ordem,sizeof(ordem),teste);
-        sscanf(ordem, "%d %d", &m, &n);
-        n = n*2;
-        char labirinto[m][n];
-
-        //For para ler e armazenar na matriz os caracteres do arquivo
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                labirinto[i][j] = fgetc(teste);
-            }
+            //Lendo a primeira linha do arquivo, para criar a matriz com a ordem informada
+            fgets(ordem,sizeof(ordem),teste);
+            sscanf(ordem, "%d %d", &m, &n);
+            n = n*2;
+            armazenar_labirinto(teste, m, n);
+            //printar_labirinto(m, n, labirinto);
+            fclose(teste);
+            break;
         }
-        // Printando o labirinto, utilizando os codigos ANSI para deixar bonitinho
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                if(labirinto[i][j] =='#') printf("%s%c%s",Laranja,' ',NONE);
-                else if(labirinto[i][j] ==' '&& labirinto[i][j+1] =='#'&& labirinto[i][j-1] =='#'){
-                    printf("%s%c%s",Laranja,' ',NONE);
-                } 
-                else if(labirinto[i][j] =='$'){
-                    printf("%s%c%s",Verde,254,NONE);
-                    final.i = i;
-                    final.j = j;
-                } 
-                else if(labirinto[i][j] =='%') printf("%s%c%s",Vermelho,254,NONE);
-                else if(labirinto[i][j] =='@'){
-                    printf("%s%c%s",Amarelo,254,NONE);
-                    jogador.i = i;
-                    jogador.j = j;
-                }        
-                else {
-                    printf ("%c", labirinto[i][j]);
-                }
-            }
+        case 2: printf("Em breve"); break;
+        case 3: {
+            printf("Em breve"); break;
+            //salvar_labirinto(m, n, ordem, armazenar_labirinto(teste, m, n));
+            break;
         }
+        case 4: exit(0); break;
+    }
 
-        //Mostra a posicao inicial e final (Fins de checagem)
+
+        /*//Mostra a posicao inicial e final (Fins de checagem)
         printf("\njogador i:%d jogador j:%d\n",jogador.i,jogador.j);
         printf("\njogador final i:%d jogador final j:%d\n",final.i,final.j);
 
@@ -150,20 +134,89 @@ int main(){
                     }
                 }
                 if(jogador.i == final.i && jogador.j == final.j) break;
-        }
+                
+        }*/
+    return 0;
+}
 
-        //Salva o caminho do grande heroi no labirinto final
-        if (resp==3){
-            FILE* lab_final = fopen("lab_final.txt", "w");
-            fputs(ordem, lab_final);
-            for (i=0; i<m; i++){
-                for (j=0; j<n; j++){
-                    fputc(labirinto[i][j], lab_final);
-                }
-            }
-            fclose(lab_final);
+// OBSERVACAO: juntei leitura/armazenamento e print em uma função só temporariamente porque não conseguia passar matriz como retorno/argumento - provavelmente terei que usar ponteiro
+
+// Função para ler o arquivo e armazenar labirinto na matriz
+void armazenar_labirinto(FILE *teste, int m, int n){
+    int i, j;
+    char labirinto[m][n];
+    //For para ler e armazenar na matriz os caracteres do arquivo
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            labirinto[i][j] = fgetc(teste);
         }
-        fclose(teste);
+    }
+    //return labirinto[m][n];
+    // Printando o labirinto, utilizando os codigos ANSI para deixar bonitinho
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            if(labirinto[i][j] =='#') printf("%s%c%s",Laranja,' ',NONE);
+            else if(labirinto[i][j] ==' '&& labirinto[i][j+1] =='#'&& labirinto[i][j-1] =='#'){
+                printf("%s%c%s",Laranja,' ',NONE);
+            } 
+            else if(labirinto[i][j] =='$'){
+                printf("%s%c%s",Verde,254,NONE);
+                //final.i = i;
+                //final.j = j;
+            } 
+            else if(labirinto[i][j] =='%') printf("%s%c%s",Vermelho,254,NONE);
+            else if(labirinto[i][j] =='@'){
+                printf("%s%c%s",Amarelo,254,NONE);
+                //jogador.i = i;
+                //jogador.j = j;
+            }        
+            else {
+                printf ("%c", labirinto[i][j]);
+            }
+        }
     }
     return 0;
+}
+
+// Função para printar o labirinto
+/*
+void printar_labirinto(int m, int n, char labirinto[m][n]){
+    int i, j;
+    // Printando o labirinto, utilizando os codigos ANSI para deixar bonitinho
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            if(labirinto[i][j] =='#') printf("%s%c%s",Laranja,' ',NONE);
+            else if(labirinto[i][j] ==' '&& labirinto[i][j+1] =='#'&& labirinto[i][j-1] =='#'){
+                printf("%s%c%s",Laranja,' ',NONE);
+            } 
+            else if(labirinto[i][j] =='$'){
+                printf("%s%c%s",Verde,254,NONE);
+                //final.i = i;
+                //final.j = j;
+            } 
+            else if(labirinto[i][j] =='%') printf("%s%c%s",Vermelho,254,NONE);
+            else if(labirinto[i][j] =='@'){
+                printf("%s%c%s",Amarelo,254,NONE);
+                //jogador.i = i;
+                //jogador.j = j;
+            }        
+            else {
+                printf ("%c", labirinto[i][j]);
+            }
+        }
+    }
+    return 0;
+}*/
+
+// Função para salvar o caminho do grande heroi no labirinto final
+void salvar_labirinto(int m, int n, int ordem, char labirinto[m][n]){
+    int i, j;
+    FILE* lab_final = fopen("lab_final.txt", "w");
+    fputs(ordem, lab_final);
+    for (i=0; i<m; i++){
+        for (j=0; j<n; j++){
+                fputc(labirinto[i][j], lab_final);
+        }
+    }
+    fclose(lab_final);
 }
